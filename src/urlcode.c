86bd2d8417d54f018eeaf581f825f700957193d3
adonais <hua.andy@gmail.com>
2019-08-23 22:23:07 +0800

    重写url编码解码函数.

--- src/urlcode.c
+++ src/urlcode.c
@@ -1,96 +1,86 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include <shlwapi.h>
-#include <windows.h>
 
-static int hex2num(char c)
-{
-    if (c>='0' && c<='9') return c - '0';
-    if (c>='a' && c<='z') return c - 'a' + 10;
-    if (c>='A' && c<='Z') return c - 'A' + 10;
-
-    printf("unexpected char: %c", c);
-    return '0';
-}
-
-// 函数会替换str为encode之后的内容，所以要保证所给的str空间足够,返回值也是str
-const char* WINAPI
+// str必须有足够的空间,函数会回写str,并返回str
+// 遵循RFC 2396,把空格编码成%20,而不是+
+const char* __stdcall
 url_encode_t(char* str)
 {
+    int i;
     int j = 0;
     char ch;
-    int m_size=(int)strlen(str);
-
-    char *result = (char *)malloc(3*m_size);
-    if ((str==NULL) || (result==NULL) || (m_size==0) ) {
-        free(result);
+    char *result = NULL;
+    int m_size=0;
+    if (str == NULL)
+    {
         return NULL;
     }
-    int i;
-    for (i=0; i<m_size; ++i) {
+    m_size=(int)strlen(str);
+    if ((result = (char *)malloc(3*m_size)) == NULL)
+    {
+        return NULL;
+    }        
+    for (i=0; i<m_size; ++i) 
+    {
         ch = str[i];
-        if (((ch>='A') && (ch<'Z')) ||
-            ((ch>='a') && (ch<'z')) ||
-            ((ch>='0') && (ch<'9'))) {
+        if (((ch>='A') && (ch<='Z')) ||
+            ((ch>='a') && (ch<='z')) ||
+            ((ch>='0') && (ch<='9'))) 
+        {
             result[j++] = ch;
-        } else if (ch == ' ') {
-            result[j++] = '+';
-        } else if (ch == '.' || ch == '-' || ch == '_' || ch == '*') {
+        }
+        else if (ch == '.' || ch == '-' || ch == '_' || ch == '*') 
+        {
             result[j++] = ch;
-        } else {
+        } 
+        else 
+        {
             sprintf(result+j, "%%%02X", (unsigned char)ch);
             j += 3;
         }
     }
-
     result[j] = '\0';
     strcpy(str,result);
     free(result);
     return str;
 }
 
-
-// 这个函数会替换str为encode之后的内容，返回值也是str
-const char* WINAPI
+// 注意,函数会回写str,并返回str
+const char* __stdcall
 url_decode_t(char* str)
 {
-    char ch,ch1,ch2;
-    int i;
-    int j = 0;
-
-    int m_size = (int)strlen(str);
-
-    char* result = (char*)malloc(m_size);
-    if ((str==NULL) || (result==NULL) || (m_size<=0) ) {
-        free(result);
-        return 0;
+    int m_size=0;
+    char *result = NULL;
+    char *output = NULL;
+    char *input = str;
+    if (str == NULL)
+    {
+        return NULL;
     }
-
-    for ( i=0; i<m_size; ++i) {
-        ch = str[i];
-        switch (ch) {
-        case '+':
-            result[j++] = ' ';
-            break;
-        case '%':
-            if (i+2<m_size) {
-                ch1 = hex2num(str[i+1]);//高4位
-                ch2 = hex2num(str[i+2]);//低4位
-                if ((ch1!='0') && (ch2!='0'))
-                    result[j++] = (char)((ch1<<4) | ch2);
-                i += 2;
-                break;
-            } else {
-                break;
-            }
-        default:
-            result[j++] = ch;
-            break;
-        }
+    if ((m_size=(int)strlen(str)) < 3)
+    {
+        return str;
     }
-    result[j] = 0;
-    strcpy(str,result);
-    free(result);
+    if ((output = result = (char *)malloc(m_size+1)) == NULL)
+    {
+        return NULL;
+    }  
+	while(*input)
+	{
+		if(*input == '%')
+		{
+			char buffer[3] = { input[1], input[2], 0 };
+			*result++ = (char)strtol(buffer, NULL, 16);
+			input += 3;
+		}
+		else
+		{
+			*result++ = *input++;
+		}
+	}
+    *result = 0;
+    strcpy(str,output);
+    free(output);
     return str;
 }
